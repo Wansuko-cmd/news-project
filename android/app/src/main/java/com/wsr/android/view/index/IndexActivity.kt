2459d9292cd58ab8acac14127739cc72d7465b56
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-07-08 14:27:07 +0900

    [add] indexActivityを追加

---
+++ android/app/src/main/java/com/wsr/android/view/index/IndexActivity.kt
@@ -0,0 +1,13 @@
+package com.wsr.android.view.index
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wsr.android.R
+
+class IndexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_index)
+    }
+}
